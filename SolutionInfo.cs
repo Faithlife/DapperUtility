23094f9ba950504d0fcb940c00c5079ab1af6f9f
Ed Ball <ejball@gmail.com>
2016-07-13 08:24:18 -0700

    Release 0.1.3.

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.2.0")]
+[assembly: AssemblyVersion("0.1.3.0")]
 [assembly: AssemblyCompany("Faithlife Corporation")]
 [assembly: AssemblyCopyright("Copyright 2016 Faithlife Corporation")]
 [assembly: AssemblyTrademark("")]
