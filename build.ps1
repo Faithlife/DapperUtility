eb79707a2f6773c0a7e4fbee27d9e5017392fb7a
Ed Ball <ejball@gmail.com>
2016-07-15 12:58:24 -0700

    Support .NET Core.

--- build.ps1
+++ build.ps1
@@ -9,8 +9,21 @@ Param(
     [string[]]$ScriptArgs
 )
 
-# download nuget.exe if not in path and not already downloaded
+# create tools directory
 $ToolsDirPath = Join-Path $PSScriptRoot "tools"
+New-Item -Path $ToolsDirPath -Type Directory -ErrorAction SilentlyContinue | Out-Null
+
+# create packages.config
+$PackagesConfigPath = Join-Path $ToolsDirPath "packages.config"
+If (!(Test-Path $PackagesConfigPath)) {
+    [System.IO.File]::WriteAllLines($PackagesConfigPath, @(
+        "<?xml version=`"1.0`" encoding=`"utf-8`"?>",
+        "<packages>",
+        "`t<package id=`"Cake`" version=`"0.14.0`" />",
+        "</packages>"))
+}
+
+# download nuget.exe if not in path and not already downloaded
 $NuGetExe = Get-Command "nuget.exe" -ErrorAction SilentlyContinue
 If ($NuGetExe -ne $null) {
     $NuGetExePath = $NuGetExe.Path
@@ -24,7 +37,7 @@ Else {
 
 # use NuGet to download Cake
 Push-Location $ToolsDirPath
-Invoke-Expression "&`"$NuGetExePath`" install -ExcludeVersion -OutputDirectory `"$ToolsDirPath`""
+Invoke-Expression "&`"$NuGetExePath`" install -ExcludeVersion -OutputDirectory ."
 If ($LASTEXITCODE -ne 0) {
     Throw "An error occured while restoring NuGet tools."
 }
